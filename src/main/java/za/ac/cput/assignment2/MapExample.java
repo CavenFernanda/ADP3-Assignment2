0e188d7b2421d1b4e5fb251c09b737e37860ca83
Caven Fernanda <fernanda17caven@gmail.com>
2021-05-14 17:01:25 +0200

    Completed methods for hashmap

--- src/main/java/za/ac/cput/assignment2/MapExample.java
+++ src/main/java/za/ac/cput/assignment2/MapExample.java
@@ -1,4 +1,19 @@
 package za.ac.cput.assignment2;
 
+import java.util.HashMap;
+
 public class MapExample {
+    private HashMap<String,Integer> gameLevel = new HashMap<String,Integer>();
+
+    public void add(String key, int level){
+        gameLevel.put(key, level);
+    }
+
+    public void remove(String key){
+        gameLevel.remove(key);
+    }
+
+    public void find(String name){
+        gameLevel.get(name);
+    }
 }
