38b9fe4e99f461059dd5f9e48ac80a8188934597
Caven Fernanda <fernanda17caven@gmail.com>
2021-05-14 14:49:53 +0200

    Created classes( test classes) for collection, map, set

---
+++ src/main/java/za/ac/cput/assignment2/SetExample.java
@@ -0,0 +1,4 @@
+package za.ac.cput.assignment2;
+
+public class SetExample {
+}
---
+++ src/test/java/za/ac/cput/assignment2/CollectionExampleTest.java
@@ -0,0 +1,7 @@
+package za.ac.cput.assignment2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollectionExampleTest {
+
+}
