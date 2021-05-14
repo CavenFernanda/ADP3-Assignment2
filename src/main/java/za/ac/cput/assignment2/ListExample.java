870bc515879d43f3966f13b40b65da7accc50643
Caven Fernanda <fernanda17caven@gmail.com>
2021-05-14 14:44:09 +0200

    Completed test cases for linked list (add, find, remove)

--- src/main/java/za/ac/cput/assignment2/ListExample.java
+++ src/main/java/za/ac/cput/assignment2/ListExample.java
@@ -1,4 +1,7 @@
 package za.ac.cput.assignment2;
+
+import java.util.LinkedList;
+
 /***
  *
  * @author Caven Fernanda - 218074905
@@ -6,5 +9,26 @@ package za.ac.cput.assignment2;
  * Adds, removes and selects element within a ArrayList
  * */
 public class ListExample {
+    LinkedList<Integer> age = new LinkedList<Integer>();
+
+    public void addAge(int a,int b){
+        age.add(a);
+        age.add(b);
+        System.out.println(age.size());
+    }
+
+    public void removeAge(){
+        age.add(5);
+        age.add(8);
+        age.add(7);
+        age.remove(1);
+    }
+
+    public void findAge(){
+        age.add(5);
+        age.add(8);
+        age.add(7);
+        Integer find = (int)age.get(0);
+    }
 
 }
--- src/test/java/za/ac/cput/assignment2/ListExampleTest.java
+++ src/test/java/za/ac/cput/assignment2/ListExampleTest.java
@@ -1,7 +1,54 @@
 package za.ac.cput.assignment2;
 
+/***
+ *
+ * @author Caven Fernanda - 218074905
+ *
+ * This class Tests Add, remove and find methods of List
+ * */
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.LinkedList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListExampleTest {
 
+    private LinkedList list;
+
+    @BeforeEach
+    void setList(){
+        list = new LinkedList();
+    }
+
+    @Test
+     void testEmptyList(){
+        assertEquals(0,list.size());
+    }
+
+    @Test
+    void testAdd(){
+        list.add(5);
+        list.add(8);
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    void testRemove(){
+        list.add(5);
+        list.add(8);
+        list.add(7);
+        list.remove(1);
+        assertEquals(2,list.size());
+    }
+
+    @Test
+    void testFind(){
+        list.add(5);
+        list.add(8);
+        list.add(7);
+        Integer find = (int)list.get(0);
+        assertEquals(5,find);
+    }
 }
