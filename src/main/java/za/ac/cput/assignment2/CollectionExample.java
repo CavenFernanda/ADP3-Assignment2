27724cd6e182b90287a8cbb79796b5a2746a3e15
Caven Fernanda <fernanda17caven@gmail.com>
2021-05-15 12:49:57 +0200

    Created test methods for setCollection

--- src/main/java/za/ac/cput/assignment2/CollectionExample.java
+++ src/main/java/za/ac/cput/assignment2/CollectionExample.java
@@ -1,4 +1,27 @@
 package za.ac.cput.assignment2;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class CollectionExample {
+    private Collection <Integer>value = new ArrayList();
+
+    public void addAge(int a,int b){
+        value.add(a);
+        value.add(b);
+        System.out.println(value.size());
+    }
+
+    public void removeAge(){
+        value.add(5);
+        value.add(8);
+        value.add(7);
+        value.remove(1);
+    }
+
+    public void findAge(){
+        value.add(5);
+        value.add(8);
+        value.add(7);
+    }
 }
--- src/test/java/za/ac/cput/assignment2/CollectionExampleTest.java
+++ src/test/java/za/ac/cput/assignment2/CollectionExampleTest.java
@@ -1,7 +1,50 @@
 package za.ac.cput.assignment2;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CollectionExampleTest {
+class CollectionExampleTest <Integer>{
+    private Collection value;
 
+    @BeforeEach
+    void setCollection(){
+        value = new ArrayList();
+    }
+
+    @Test
+    void testSet(){
+        assertEquals(0,value.size());
+    }
+
+    @Test
+    void testAdd(){
+        value.add(5);
+        value.add(145);
+        value.add(74);
+
+        assertEquals(3,value.size());
+    }
+
+    @Test
+    void testRemove(){
+        value.add(5);
+        value.add(145);
+        value.add(74);
+        value.remove(0);
+
+        assertEquals(2,value.size());
+    }
+
+    @Test
+    void testFind(){
+        value.add(5);
+        value.add(145);
+        value.add(74);
+
+        assertTrue(value.contains(5));
+    }
 }
