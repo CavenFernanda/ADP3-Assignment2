c4d125e46a6be9fb242f8c51f431b7d431e25dd6
Caven Fernanda <fernanda17caven@gmail.com>
2021-05-15 11:34:33 +0200

    Created test methods for SetExample

--- src/main/java/za/ac/cput/assignment2/SetExample.java
+++ src/main/java/za/ac/cput/assignment2/SetExample.java
@@ -1,4 +1,27 @@
 package za.ac.cput.assignment2;
+import java.util.HashSet;
 
 public class SetExample {
+    HashSet<String> city = new HashSet<String>();
+
+    public void addAge(String a,String b){
+        city.add(a);
+        city.add(b);
+        System.out.println(city.size());
+    }
+
+    public void removeAge(){
+        city.add("Cape Town");
+        city.add("Durban");
+        city.add("Johannesburg");
+        city.remove("Durban");
+    }
+
+    public void findAge(){
+        city.add("Cape Town");
+        city.add("Durban");
+        city.add("Johannesburg");
+        System.out.println(city.contains("Durban"));
+    }
+
 }
--- src/test/java/za/ac/cput/assignment2/SetExampleTest.java
+++ src/test/java/za/ac/cput/assignment2/SetExampleTest.java
@@ -1,7 +1,48 @@
 package za.ac.cput.assignment2;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
 
 class SetExampleTest {
+    private  HashSet<String> city;
 
+    @BeforeEach
+    void setStructure(){
+        city = new HashSet<String>();
+    }
+
+    @Test
+    void testSet(){
+        assertEquals(0, city.size());
+    }
+
+    @Test
+    void testAdd(){
+        city.add("Cape Town");
+        city.add("Durban");
+        city.add("Johannesburg");
+
+        assertEquals(3, city.size());
+    }
+
+    @Test
+    void testRemove(){
+        city.add("Cape Town");
+        city.add("Durban");
+        city.add("Johannesburg");
+        city.remove("Durban");
+        assertEquals(2,city.size());
+    }
+
+    @Test
+    void testFind(){
+        city.add("Cape Town");
+        city.add("Durban");
+        city.add("Johannesburg");
+
+        assertTrue(city.contains("Durban"));
+
+    }
 }
